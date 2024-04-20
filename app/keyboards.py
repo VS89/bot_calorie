ab47244c7f905c48d8e3a2d28bfd6657956219c7
Valentin Semenov <valentins@MacBook-Pro-Valentin.local>
2024-04-20 13:02:26 +0300

    add handler command /start

--- app/keyboards.py
+++ app/keyboards.py
@@ -1,3 +1,35 @@
-"""
-Тут описание клавиатур
-"""
+import json
+
+from math import ceil
+from pydantic import BaseModel, Field
+
+
+class InlineKeyboardButtonModel(BaseModel):
+    """
+    Inline кнопка для клавиатуры
+    """
+
+    text: str = Field(...)
+    callback_data: str | None = Field(None)
+
+    @property
+    def dict_data_exclude_none(self) -> dict:
+        return self.model_dump(exclude_none=True)
+
+
+class InlineKeyboardsModel(BaseModel):
+    """
+    Inline клавиатура
+    """
+
+    rows: int = Field(1)
+
+    def create_keyboard(self, buttons: list[InlineKeyboardButtonModel]) -> json:
+        """
+        Создание клавиатуры
+        :param buttons: список кнопок
+        :return: объект json
+        """
+        part = ceil(len(buttons)/self.rows)
+        new_buttons = [i.dict_data_exclude_none for i in buttons]
+        return json.dumps({'inline_keyboard': [new_buttons[part * k:part * (k + 1)] for k in range(self.rows)]})
--- app/main.py
+++ app/main.py
@@ -7,6 +7,10 @@ import uvloop
 from fastapi import FastAPI
 from starlette.requests import Request
 
+from app.handler_commands.command_start import get_start_message
+from app.keyboards import InlineKeyboardsModel, InlineKeyboardButtonModel
+from app.models.telegram_response_models import CallbackQueryModel, MessageModel, TelegramResponse, EntitiesType
+
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 # logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.DEBUG)
 
@@ -23,44 +27,66 @@ app = FastAPI()
 # создание кнопок
 replay_markup = json.dumps({
     "inline_keyboard": [
-      [
-        {
-          "text": "Кнопка 1",
-          "callback_data": "callback text 1"
-        },
-        {
-          "text": "Кнопка 2",
-          "callback_data": "callback text 2"
-        }
-      ]
+        [
+            {
+                "text": "Кнопка 1",
+                "callback_data": "callback text 1"
+            },
+            {
+                "text": "Кнопка 2",
+                "callback_data": "callback text 2"
+            }
+        ]
     ]
-  })
+})
 
 
 @app.post(f"/webhook{TOKEN}")
 async def webhook(req: Request):
     logging.info("Я зашел в вебхук")
     data = await req.json()
+    logging.info(f"{data=}")
+    response_model = TelegramResponse(**data)
     try:
-        if data.get('callback_query'):
-            if data['callback_query'].get('data') == 'callback text 2':
-                chat_id = data['callback_query']['message']['chat']['id']
-                text = "Вы нажали кнопку 2"
-        else:
-            chat_id = data['message']['chat']['id']
-            text = data['message']['text']
-
-        logging.info(f'мой чат ид == {chat_id}')
         # await client.get(f"{BASE_URL}/sendMessage?chat_id={chat_id}&text={text}")
         # todo надо разобраться как крепить кнопки к сообщению через апи
         # Prepare the data payload
-        data = {
-            "chat_id": chat_id,
-            "text": text,
-            "reply_markup": replay_markup
-        }
-        resp = await client.post(f"{BASE_URL}/sendMessage", data=data)
-        logging.info(resp.json())
+        if response_model.callback_query:
+            resp_data = {
+                'chat_id': response_model.callback_query.message.chat.user_id,
+                'text': "Я на что-то нажал"
+            }
+            await client.post(f"{BASE_URL}/sendMessage", data=resp_data)
+            return
+        if response_model.message.entities:
+            text = 'Неожиданный entity для меня'
+            if response_model.message.entities[0].type == EntitiesType.BOT_COMMAND.value:
+                match response_model.message.text:
+                    case '/start':
+                        text = get_start_message()
+                    case _:
+                        text = 'Пока не знаю кейс для этой команды'
+            resp_data = {
+                'chat_id': response_model.message.chat.user_id,
+                'text': text
+            }
+            await client.post(f"{BASE_URL}/sendMessage", data=resp_data)
+            return
+        else:
+            logging.info(f"{response_model=}")
+            resp_data = {
+                "chat_id": response_model.message.chat.user_id,
+                "text": "Какое-то сообщение",
+                "reply_markup": InlineKeyboardsModel(rows=1).create_keyboard(buttons=[
+                    InlineKeyboardButtonModel(text='1', callback_data='1'),
+                    InlineKeyboardButtonModel(text='2', callback_data='callback text 2'),
+                    InlineKeyboardButtonModel(text='3', callback_data='1'),
+                    InlineKeyboardButtonModel(text='4', callback_data='1'),
+                    InlineKeyboardButtonModel(text='5', callback_data='1'),
+                ])
+            }
+            await client.post(f"{BASE_URL}/sendMessage", data=resp_data)
+            return
     except Exception as e:
         logging.error(e)
 
@@ -69,7 +95,7 @@ async def webhook(req: Request):
 
 # tg id 281626882
 if __name__ == '__main__':
-    tuna_url = "https://77kw5l-31-134-187-85.ru.tuna.am"
+    tuna_url = "https://prohxf-31-134-187-85.ru.tuna.am"
     # todo запрос нужен чтобы заработал вебхук, его надо перенести в startup event
     resp = requests.get(url=f"https://api.telegram.org/bot{TOKEN}/setWebhook?url={tuna_url}/webhook{TOKEN}")
     logging.info(f"Ответ от метода установки хука для телеги: {resp.json()}")
